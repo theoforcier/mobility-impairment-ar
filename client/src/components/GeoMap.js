371ec51ea29a899dfeaf87ea1fe8e365c32979c3
William Wu <45546188+wxwilliam@users.noreply.github.com>
2023-02-05 21:03:32 -0500

    working map code, live coordinate underdevelopment

--- client/src/components/GeoMap.js
+++ client/src/components/GeoMap.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
 
 const GeoMap = () => {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
@@ -17,11 +18,16 @@ const GeoMap = () => {
 
   return (
     
-    <MapContainer center={[location.lat, location.lng]} zoom={13}>
+    <MapContainer center={[42.984268, -81.247528]} zoom={13}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={[42.984268, -81.247528]}>
+      <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
     </MapContainer>
   );
 }
